9297551fd2929b6728688cbb57c55958b80172e2
sopandobhal <sopandobhal@gmail.com>
2021-02-03 17:43:41 +0530

    Removes disabled code

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const fs = require('fs-extra')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
-// setup initial global state
-// let state = getEmptyState()
-
 let hangUpScript = [{"Hangup": {"callId": "replaceVal:CallId", "reason": "timeout"}}]
 let recordUtteranceScript = [
    {
@@ -28,11 +25,6 @@ let recordUtteranceScript = [
 // Create server
 const app = express()
 
-// //Log when request received
-// app.use(morgan('[:date] :method :url :status :res[content-length] - :response-time ms', {immediate: true}))
-// //Log when request responds
-// app.use(morgan('[:date] :method :url :status :res[content-length] - :response-time ms', {immediate: false}))
-
 
 app.use(bodyParser.json())
 
@@ -51,61 +43,5 @@ app.post('/hangup', function (req, res){
 
 	res.send(hangUpScript)
 })
-
-// app.post('/', function (req, res) {
-// 	console.log(`new call received: ${JSON.stringify(req.body)}`)
-// 	state.vallReceived = true
-// 	state.callId = req.body.callId
-// 	state.accountId = req.body.accountId
-
-// 	state.percl = replaceVal(state.percl)
-
-// 	updateCallStatusObject(req.body.callId, req.body.callStatus, req.body.callDuration, req.body, parentCallId)
-// 	console.log(`call status: ${JSON.stringify(state.callStatuses)}`)
-
-// 	res.send(state.percl)
-// })
-
-// app.put('/', function (req, res) {
-// 	console.log(`Setting up percl script in state: ${JSON.stringify(JSON.stringify(req.body))}`)
-// 	state.percl = req.body
-// 	state.callConnectPercl = req.body
-
-// 	res.sendStatus(204)
-// })
-
-// app.get('/', function (req, res) {
-// 	console.log(`retriving current percl script: ${JSON.stringify(state.percl)}`)
-// 	state.percl = replaceVal(state.percl, "callId")
-// 	res.send(state.percl)
-// })
-
-
-
-// app.post('/action', function (req, res){
-// 	console.log(`Action url invoked: ${JSON.stringify(req.body)}`)
-
-// 	updateCallStatusObject(req.body.callId, req.body.callStatus, req.body.callDuration, req.body, parentCallId)
-
-// 	if(req.body.recordingUrl !== undefied) {
-// 		state.latestRecordingUrl = req.body.recordingUrl
-// 		console.log(`recordingUrl updated: ${req.body.recordingUrl}`)
-// 	}
-
-// 	state.actionScript = replaceVal(state.actionScript)
-// 	res.send(state.actionScript)
-// })
-
-// app.get('/action', function (req, res) {
-// 	console.log(`retriving action script: ${JSON.stringify(state.actionScript)}`)
-// 	res.send(state.actionScript)
-// })
-
-// app.put('/action', function (req, res) {
-// 	console.log(`Setting up action script in state: ${JSON.stringify(JSON.stringify(req.body))}`)
-// 	state.actionScript = req.body
-
-// 	res.sendStatus(204)
-// })
 
 module.exports = app
